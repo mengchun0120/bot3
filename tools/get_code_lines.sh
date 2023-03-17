21f68b36b3b545d928dd682aff9db82e12dc92ba
Chun Meng <mengchun0120@gmail.com>
2023-03-17 15:51:10 -0400

    Updated get_code_lines.sh

--- tools/get_code_lines.sh
+++ tools/get_code_lines.sh
@@ -1,1 +1,9 @@
-find . -name "*.cpp" -or -name "*.h" | xargs wc -l | grep total
+folders="commonlib botlib desktop"
+
+sum=0
+for folder in $folders; do
+    x=$(find $folder -name "*.cpp" -or -name "*.h" | xargs wc -l | grep total | awk '{print $1}')
+    sum=$(expr $sum + $x)
+done
+
+echo $sum
