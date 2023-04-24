460e162fc475b0ba33448275e12e71d0918c664a
Chun Meng <mengchun0120@gmail.com>
2023-04-24 10:11:21 -0400

    Changed SkillButton size & positions

--- tools/gen_progress_pie.sh
+++ tools/gen_progress_pie.sh
@@ -1,3 +1,3 @@
-python tools/gen_progress_pie.py --start-angle 90 --clockwise True --radius 50.0 --num-edges 360 --has-texture False --prefix skill_progress_pie
+python tools/gen_progress_pie.py --start-angle 90 --clockwise True --radius 80.0 --num-edges 360 --has-texture False --prefix skill_progress_pie
 #python tools/gen_progress_pie.py --start-angle 90 --clockwise True --radius 50.0 --num-edges 360 --has-texture False --prefix goodie_progress_pie
 
